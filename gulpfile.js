95a7f8fa9236afa4e1c0195e2482c600e5a804b1
shanfelei <1628316216@qq.com>
2018-09-16 23:16:46 +0800

    ‘顶部

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ const webpack = require('webpack-stream')
 const inject = require('gulp-inject')
 // 编译sass
 const sass = require('gulp-sass')
-
+//压缩图片
+const imageMin = require('gulp-imagemin');
 // 处理html，将src中的html文件输出到dist中去
 gulp.task('handle:html', function () {
     return gulp.src('./src/views/*/*.html')
@@ -32,6 +33,11 @@ gulp.task('handle:html', function () {
         .pipe(gulp.dest('./dist'))
 })
 
+gulp.task('image', function () {
+    return gulp.src('./src/image/*.*')
+        .pipe(imageMin({progressive: true}))
+        .pipe(gulp.dest('./dist/image'))
+})
 // 处理css， 合并css， 压缩css， 前缀，输出
 gulp.task('handle:css', function () {
     // 1. 希望可以合并成多个css，更灵活  2. 多页面灵活处理
@@ -56,38 +62,11 @@ gulp.task('handle:css', function () {
         }
     }
     return merge( ...streams )//合并多个文件流
-    // ... 是es6 中的展开运算符 var  a = [1,2,3,4]  var b = [ ...a, 5,6,7] // b: [1,2,3,4,5,6,7]
-    // var a = { x: 1, y: 2 } var b = { z: 3, ...a } // b: { x: 1, y: 2, z: 3 }
 })
 
 // 处理js es6-> es5 合并 压缩
 
 gulp.task('handle:js', function () {
-    // return gulp.src('src/entry.js')//这里瞎写一个就行
-    //     //真正的处理都是在这里的
-    //     .pipe(webpack({
-    //         mode: 'production', // 设置打包模式： none development production(会压缩代码)
-    //         //单入口 单出口
-    //         // entry: './src/views/index/javascripts/index.js',// 入口
-    //         // output: {
-    //         //     filename: 'index.js'
-    //         // }
-    //         //多入口 单出口 数组中，谁在前面，打包的时候也在前面
-    //         // entry: ['./src/views/index/javascripts/index.js','./src/views/index/javascripts/vendor.js'],// 入口
-    //         // output: {
-    //         //     filename: 'index.js'
-    //         // }
-    //         // 多入口 多出口
-    //         entry: {
-    //             index: './src/views/index/javascripts/index.js',
-    //             vendor: './src/views/index/javascripts/vendor.js'
-    //         }, // 入口
-    //         output: {
-    //             filename: '[name].min.js' // [name]代表在entry中键名是什么，打包出来的就是什么
-    //         }
-    //     }))
-    //     .pipe(gulp.dest('./dist/index/js'))
-
     let streams = []
     for (const page in config.jsoptions) {
         //判断如果入口是数组或者是字符串的话就是单出口，否则是多出口
@@ -140,6 +119,7 @@ gulp.task('watch', function () {
     gulp.watch('./src/views/*/*.html', ['handle:html', 'inject', 'reload'])
     gulp.watch('./src/**/*.scss', ['handle:css', 'inject', 'reload'])
     gulp.watch('./src/**/*.js', ['handle:js', 'inject', 'reload'])
+    gulp.watch('./src/image/*.*', ['image', 'inject', 'reload'])
     // 通配符中 * 指的是儿子这一代，** 指的是所有的后代
 })
 
@@ -155,4 +135,4 @@ gulp.task("reload", function(){
 })
 
 // 默认任务
-gulp.task('default', ['server', 'handle:html', 'handle:css', 'handle:js', 'inject', 'watch'])
+gulp.task('default', ['server', 'handle:html', 'handle:css', 'handle:js','image', 'inject', 'watch'])
