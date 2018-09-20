e50f2639c8aff23deb9806eab9f1cb392dd2648c
shanfelei <1628316216@qq.com>
2018-09-20 19:33:42 +0800

    选项卡

--- src/views/index/javascripts/index.js
+++ src/views/index/javascripts/index.js
@@ -18,5 +18,21 @@ $(function(){
 			$('.car-on').css('display','none')
 		})
 		})
-		
+				//为li添加鼠标移入事件
+	//选择器.事件( function(){ ... } )
+	$(".tab-item").mouseenter( function(){
+		//操作当前标题有样式  当前标题的所有兄弟没有样式 siblings
+		//console.log( this );		
+		$(this).addClass("active")
+			   .siblings()
+			   .removeClass("active");
+	
+		//内容  
+		//获取当前标题的下标  根据下标找内容
+		var index = $(this).index();
+		$(".products div").eq(index)
+		                  .addClass("selected")
+		                  .siblings()
+		                  .removeClass("selected");
+	} )
 })
